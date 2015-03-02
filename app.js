42ae186ed6690232ad500df909e69a2686a72250
unknown <pchriste@PIN1258007.hsr.ch>
2015-03-02 16:16:02 +0100

    bower-test

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 app.use('/', express.static(__dirname + '/app'));
+
+//add this so the browser can GET the bower files
+app.use('/bower_components', express.static(__dirname + '/app/bower_components'));
+
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() { 
     console.log('Your files will be served through this web server')

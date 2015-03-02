011ef09e79d75456b29dbe80ca511000a28796de
unknown <pchriste@PIN1258007.hsr.ch>
2015-03-02 16:18:31 +0100

    test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 app.use('/', express.static(__dirname + '/app'));
 
 //add this so the browser can GET the bower files
-app.use('/bower_components', express.static(__dirname + '/app/bower_components'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() { 

6b53a08609034b2340be1c8d684a00291cb29304
unknown <pchriste@PIN1258007.hsr.ch>
2015-03-02 16:13:05 +0100

    test

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-app.use('/', express.static(__dirname + '/'));
+app.use('/', express.static(__dirname + '/app'));
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() { 
     console.log('Your files will be served through this web server')

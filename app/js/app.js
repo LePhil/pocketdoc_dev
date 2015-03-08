7a93c5a8a0cb1b2de551dbbfe8b2154dfd767a5d
LePhil <philipp.christen@gmail.com>
2015-03-08 09:38:03 +0100

    first pocketdoc tries

--- app/js/app.js
+++ app/js/app.js
@@ -2,27 +2,7 @@
 
 /* App Module */
 
-var phonecatApp = angular.module('phonecatApp', [
-  'ngRoute',
-  'phonecatAnimations',
-
-  'phonecatControllers',
-  'phonecatFilters',
-  'phonecatServices'
+var pocketdocApp = angular.module('pocketdocApp', [
+  "ngMaterial",
+  "pocketdocControllers"
 ]);
-
-phonecatApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/phones', {
-        templateUrl: 'partials/phone-list.html',
-        controller: 'PhoneListCtrl'
-      }).
-      when('/phones/:phoneId', {
-        templateUrl: 'partials/phone-detail.html',
-        controller: 'PhoneDetailCtrl'
-      }).
-      otherwise({
-        redirectTo: '/phones'
-      });
-  }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,22 +1,48 @@
 'use strict';
 
 /* Controllers */
+var pocketdocControllers = angular.module('pocketdocControllers', []);
 
-var phonecatControllers = angular.module('phonecatControllers', []);
+pocketdocControllers.controller('AppCtrl', function($scope) {
+    $scope.questions = [
+      {
+        'id'     : 0,
+        'status' : 'unanswered',
+        'subject': 'Fieber?',
+        'text'   : 'Haben Sie Fieber?'
+      },
+      {
+        'id'     : 1,
+        'status' : 'unanswered',
+        'subject': 'Hohes Fieber?',
+        'text'   : 'Ist Ihr Fieber über 38.5 °C ?'
+      },
+      {
+        'id'     : 2,
+        'status' : 'unanswered',
+        'subject': 'Sehr hohes Fieber?',
+        'text'   : 'Ist Ihr Fieber über 41.1 °C ?'
+      },
+      {
+        'id'     : 3,
+        'status' : 'unanswered',
+        'subject': 'Bauchschmerzen?',
+        'text'   : 'Haben Sie Bauchschmerzen?'
+      },
+      {
+        'id'     : 4,
+        'status' : 'unanswered',
+        'subject': 'Blähungen?',
+        'text'   : 'Haben Sie Blähungen?'
+      },
+    ];
 
-phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
-  function($scope, Phone) {
-    $scope.phones = Phone.query();
-    $scope.orderProp = 'age';
-  }]);
-
-phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
-  function($scope, $routeParams, Phone) {
-    $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
-      $scope.mainImageUrl = phone.images[0];
-    });
-
-    $scope.setImage = function(imageUrl) {
-      $scope.mainImageUrl = imageUrl;
+    $scope.answerQuestion = function( question, answer ) {
+      console.log(question, answer);
+      question.status = answer;
     }
-  }]);
+
+    $scope.isAnswered = function( question ) {
+      return question.status === "unanswered";
+    }
+});

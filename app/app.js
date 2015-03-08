42ec8821063490f99215bcf5929562b5b0a22744
pchriste <pchriste@hsr.ch>
2015-03-08 11:36:35 +0100

    bower_components folder added

--- app/app.js
+++ app/app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 app.use('/', express.static(__dirname + '/app'));
+
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() { 
     console.log('Your files will be served through this web server')
